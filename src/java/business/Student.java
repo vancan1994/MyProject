cc4e978f0f301aa01980c75629e309c68f346629
iviettech <iviettech@iviettech-PC>
2017-11-28 08:32:14 +0700

    this is first version

---
+++ src/java/business/Student.java
@@ -0,0 +1,16 @@
+package business;
+public class Student {
+    private int studentID;
+    private String studentName;
+
+    public Student() {
+    }
+    //This is fist
+    public Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    
+    
+}
