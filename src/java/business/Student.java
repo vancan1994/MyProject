5139e157d0960d115f01a37f5ae002035664f04c
iviettech <iviettech@iviettech-PC>
2017-11-28 08:36:51 +0700

    v2

--- src/java/business/Student.java
+++ src/java/business/Student.java
@@ -11,6 +11,20 @@ public class Student {
         this.studentName = studentName;
     }
 
-    
-    
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+   
 }
